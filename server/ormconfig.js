7b1785bdb7350dd1104cc3979786635c1c2ba415
unknown <landon3388@gmail.com>
2021-11-19 17:41:18 -0500

    red

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,24 +1,20 @@
+let ruta = "src";
+
 module.exports = {
-   "type": process.env.DB_DIALECT,
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "username": process.env.DB_USERNAME,
-   "password": process.env.DB_PASSWORD,
-   "database":process.env.DB_DATABASE,
-   "synchronize": true,
-   "logging": false,
-   "entities": [
-      "dist/entity/**/*{.ts,.js}"
-   ],
-   "migrations": [
-      "dist/migration/**/*{.ts,.js}"
-   ],
-   "subscribers": [
-      "dist/subscriber/**/*{.ts,.js}"
-   ],
-   "cli": {
-      "entitiesDir": "dist/entity",
-      "migrationsDir": "dist/migration",
-      "subscribersDir": "dist/subscriber"
-   }
-}
+  type: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [ruta + "/entity/**/*{.ts,.js}"],
+  migrations: [ruta + "/migration/**/*{.ts,.js}"],
+  subscribers: [ruta + "/subscriber/**/*{.ts,.js}"],
+  cli: {
+    entitiesDir: ruta + "/entity",
+    migrationsDir: ruta + "/migration",
+    subscribersDir: ruta + "/subscriber",
+  },
+};
